fca3d38f8001176e655d33b1c29a712c81cae346
Aj <ablosser@wvuf.org>
2020-08-08 14:37:06 -0400

    rc1

---
+++ precache-manifest.14b2775c14f2a77341e14662c5d115e9.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "00a61000ebfc6419010ca8c4f1052773",
+    "url": "/index.html"
+  },
+  {
+    "revision": "787b2d6b33d45b2ce32b",
+    "url": "/static/css/2.f005c5ff.chunk.css"
+  },
+  {
+    "revision": "45e1633bde5582b265ef",
+    "url": "/static/css/main.0fca2065.chunk.css"
+  },
+  {
+    "revision": "787b2d6b33d45b2ce32b",
+    "url": "/static/js/2.30d9f46b.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/static/js/2.30d9f46b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "45e1633bde5582b265ef",
+    "url": "/static/js/main.d7b90558.chunk.js"
+  },
+  {
+    "revision": "a2728163426bccc46053",
+    "url": "/static/js/runtime-main.76bf677e.js"
+  },
+  {
+    "revision": "ca1b5b1592ed2da11c713c8e2b8928d5",
+    "url": "/static/media/bg.ca1b5b15.jpg"
+  },
+  {
+    "revision": "8481a6987062b787f02f29e98e18954e",
+    "url": "/static/media/logo2.8481a698.png"
+  }
+]);
